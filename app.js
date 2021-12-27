fb603ccf107646c8204c7b5fa5b9c6428318da8b
nedimbatilovic <nedimco2006@gmail.com>
2021-12-27 16:51:26 +0100

    hue slider, slider styling

--- app.js
+++ app.js
@@ -45,10 +45,31 @@ function colorizeSliders(color, hue, brightness, saturation) {
     const fullSat = color.set("hsl.s", 1);
     const scaleSat = chroma.scale([noSat, color, fullSat]);
 
+    const midBright = color.set("hsl.l", 0.5);
+    const scaleBright = chroma.scale(["black", midBright, "white"]);
+
     saturation.style.backgroundImage = `linear-gradient(
         to right, 
         ${scaleSat(0)}, 
         ${scaleSat(1)}
+    )`;
+
+    brightness.style.backgroundImage = `linear-gradient(
+        to right, 
+        ${scaleBright(0)}, 
+        ${scaleBright(0.5)}, 
+        ${scaleBright(1)}
+    )`;
+
+    hue.style.backgroundImage = `linear-gradient(
+        to right,
+        rgb(204, 75, 75),
+        rgb(204, 204, 75),
+        rgb(75, 204, 75),
+        rgb(75, 204, 204),
+        rgb(75, 75, 204),
+        rgb(204, 75, 204),
+        rgb(204, 75, 75)
     )`;
 }
 
