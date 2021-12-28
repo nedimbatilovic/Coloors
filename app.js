481dbcc56e0878ee533ea795bbeadf47cd7899fb
nedimbatilovic <nedimco2006@gmail.com>
2021-12-28 07:49:32 +0100

    generate

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll(".color h2");
 const popup = document.querySelector(".copy-container");
 const adjustButton = document.querySelectorAll(".adjust");
+const lockButton = document.querySelectorAll(".lock");
 const closeAdjustments = document.querySelectorAll(".close-adjustment");
 const sliderContainers = document.querySelectorAll(".sliders");
 let initialColors;
 
 //event listeners
+generateBtn.addEventListener("click", randomColors());
+
 sliders.forEach((slider) => {
     slider.addEventListener("input", hslControls);
 });
@@ -72,6 +75,11 @@ function randomColors() {
     });
 
     resetInputs();
+
+    adjustButton.forEach((button, index) => {
+        checkTextContrast(initialColors[index], button);
+        checkTextContrast(initialColors[index], lockButton[index]);
+    });
 }
 
 function checkTextContrast(color, text) {
